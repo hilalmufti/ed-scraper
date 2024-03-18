28c24f3a2e5efefbe624f6a6cc7bcc2ea173da32
ronald <ronaldlindev@gmail.com>
2024-03-18 00:38:53 -0700

    added requirements fille

--- src/ed-scraper/scraper.py
+++ src/ed-scraper/scraper.py
@@ -9,7 +9,7 @@ import time
 STUDENT_EMAIL = "kdo55%40uw.edu"
 WEB_ADDRESS = f"https://edstem.org/us/courses/50191/lessons/87193/attempts?email={STUDENT_EMAIL}&slide=486954" # for stonks
 STAFF_EMAIL = "rlin0622@uw.edu"
-STAFF_PASSWORD = "XXXX"
+STAFF_PASSWORD = "XXX"
 TIME_OUT = 60
 
 driver = webdriver.Safari() # change this if u dont use safari
@@ -29,14 +29,13 @@ login = WebDriverWait(driver, TIME_OUT).until(lambda x: x.find_element(By.XPATH,
 login.send_keys(STAFF_PASSWORD, Keys.ENTER)
 
 # make sure you accept duo mobile
-time.sleep(30)
 
 # pressing "this is my device button"
-WebDriverWait(driver, TIME_OUT).until(lambda x: x.find_element(By.XPATH, '//*[@id="trust-browser-button"]').click())
-
-# ed scraiping
-student_code = WebDriverWait(driver, TIME_OUT * 100 ).until(lambda x: x.find_elements(By.XPATH, "/html/body/div[1]/div[1]/div/div/div/main/div/div/div/div/div[1]/div/div/div/div/div[3]/div/div/div/div[3]/div/div[4]/div/div[2]/div/div/div[1]/div/div/table"))
+button = WebDriverWait(driver, TIME_OUT).until(lambda x: x.find_element(By.XPATH, '//*[@id="trust-browser-button"]'))
+button.click()
+# ed scraping
+student_code = WebDriverWait(driver, TIME_OUT * 1000 ).until(lambda x: x.find_elements(By.XPATH, "/html/body/div[1]/div[1]/div/div/div/main/div/div/div/div/div[1]/div/div/div/div/div[3]/div/div/div/div[3]/div/div[4]/div/div[2]/div/div/div[1]/div/div/table"))
 print(student_code)
 
-student_feedback = WebDriverWait(driver, TIME_OUT * 100).until(lambda x: x.find_elements(By.CLASS_NAME, "amber-el amber-paragraph amber-content"))
+student_feedback = WebDriverWait(driver, TIME_OUT * 1000).until(lambda x: x.find_elements(By.CLASS_NAME, "amber-el amber-paragraph amber-content"))
 
